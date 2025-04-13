cffd23d0c815539137721c810cc41b5e501ac575
juancastro000 <cdyutub@gmail.com>
2025-04-13 04:19:07 +0200

    feat: add react routes and configure app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { AuthProvider } from './contexts/AuthContexts'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage';
 import Navbar from "./components/navbar/Navbar";
 import Footer from './components/foteer/footer';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import "./App.css"
 
 function App() {
   return (
-    
     <AuthProvider>
-      <div className="app-container">
-        <Navbar 
-          onLoginClick={() => setShowLoginModal(true)} 
-          onRegisterClick={() => setShowRegisterModal(true)}
-        />
-        <div className="main-content">
-          <HomePage />
+      <BrowserRouter>
+        <div className="app-container">
+        <Navbar/>
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/*<Route
+                path="/timeline"
+                element={<ProtectedRoute><TimelinePage /></ProtectedRoute>}
+              />*/}
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
