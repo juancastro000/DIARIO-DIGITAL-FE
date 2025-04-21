b62f335a5541f1545327910a9f2b98f49291d319
juancastro000 <cdyutub@gmail.com>
2025-04-21 04:26:03 +0200

    refactor: add use auth to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import LoginModal from "../login/LoginModal";
 import RegisterModal from "../register/RegisterModal";
+import { useAuth } from "../../contexts/AuthContexts";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { user } = useAuth();
   const [activeModal, setActiveModal] = useState(null);
 
   return (
@@ -12,33 +14,39 @@ const Navbar = () => {
       <div className="navbar-left">
         <h1 className="logo">Diario Digital</h1>
       </div>
-      <div className="navbar-center">
-        <ul className="nav-links">
-          <li>
-            <Link to="/timeline">Línea de Tiempo</Link>
-          </li>
-          <li>
-            <Link to="/tasks">Tareas</Link>
-          </li>
-          <li>
-            <Link to="/statistics">Estadísticas</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-right">
-        <button
-          className="btn secondary"
-          onClick={() => setActiveModal("login")}
-        >
-          Iniciar Sesión
-        </button>
-        <button
-          className="btn primary"
-          onClick={() => setActiveModal("register")}
-        >
-          Registrarse
-        </button>
-      </div>
+      {user ? (
+        <div className="navbar-center">
+          <ul className="nav-links">
+            <li>
+              <Link to="/timeline">Línea de Tiempo</Link>
+            </li>
+            <li>
+              <Link to="/tasks">Tareas</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Estadísticas</Link>
+            </li>
+          </ul>
+        </div>
+      ) : null}
+
+      {!user ? (
+        <div className="navbar-right">
+          <button
+            className="btn secondary"
+            onClick={() => setActiveModal("login")}
+          >
+            Iniciar Sesión
+          </button>
+          <button
+            className="btn primary"
+            onClick={() => setActiveModal("register")}
+          >
+            Registrarse
+          </button>
+        </div>
+      ) : null}
+
       {activeModal === "login" && (
         <LoginModal onClose={() => setActiveModal(null)} />
       )}
